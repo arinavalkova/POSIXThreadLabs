d1ab897ff2317822f6f6cb5ac830365152f270be
arinavalkova <65497338+arinavalkova@users.noreply.github.com>
2020-08-15 19:08:56 +0700

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define ERROR_CREATE_THREAD 1
-
 void printTenLines(char *line)
 {
     for(int i = 0; i < 10; i++)
@@ -27,10 +25,15 @@ int main(int argc, char** argv)
     if(createThreadStatus != 0)
     {
         printf("Main thread: can't create thread, status=%d", createThreadStatus);
-        exit(ERROR_CREATE_THREAD);
+        exit(EXIT_FAILURE);
     }
 
-    pthread_join(thread, NULL);
+    int joiningThreadStatus = pthread_join(thread, NULL);
+    if(joiningThreadStatus != 0)
+    {
+        printf("Main thread: can't join thread, status=%d", joiningThreadStatus);
+        exit(EXIT_FAILURE);
+    }
 
     printTenLines("I am a main thread...");
 

1902ed44f50bfab99ebdc7be602fd370184c8643
arinavalkova <65497338+arinavalkova@users.noreply.github.com>
2020-08-15 16:23:38 +0700

    Create lab3.c

---
+++ lab3/lab3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define ERROR_CREATE_THREAD 1
+#define COUNT_OF_THREADS 4
+#define MAX_COUNT_OF_LINES 5
+
+const char* linesForPrintInThreads[][MAX_COUNT_OF_LINES] =
+        {
+            {"line1 in thread1...", "line2 in thread2...", "line3 in thread3...", "line4 in thread4...", NULL},
+            {"line1 in thread2...", "line2 in thread2...", NULL},
+            {"line1 in thread3...", NULL},
+            {"line1 in thread4...", "line2 in thread4...", "line3 in thread4...", NULL}
+        };
+
+void* printLinesInNewThread(void* parameters)
+{
+    char** arrayOfLines;
+
+    for(arrayOfLines = (char**)parameters; *arrayOfLines != NULL; arrayOfLines++)
+    {
+        printf("%s\n", *arrayOfLines);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv)
+{
+    pthread_t thread[COUNT_OF_THREADS];
+    int createThreadStatus;
+
+
+    for(int i = 0; i < COUNT_OF_THREADS; i++)
+    {
+        createThreadStatus = pthread_create(&thread[i], NULL, printLinesInNewThread, linesForPrintInThreads[i]);
+        if(createThreadStatus != 0)
+        {
+            printf("Main thread: can't create %d thread, status=%d", i, createThreadStatus);
+            exit(ERROR_CREATE_THREAD);
+        }
+    }
+
+    printf("Main thread...\n");
+
+    pthread_exit(NULL);
+    exit(EXIT_SUCCESS);
+}

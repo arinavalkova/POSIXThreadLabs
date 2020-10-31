bd3c293a1f888fb1c61c8161ba45291d3ec6d60a
arinavalkova <65497338+arinavalkova@users.noreply.github.com>
2020-10-31 18:23:35 +0700

    Update myMsgQueue.h

--- lab23/myMsgQueue.h
+++ lab23/myMsgQueue.h
@@ -3,8 +3,8 @@
 #include "queue.h"
 
 class MyMsgQueue {
-    struct queue *next, *prev;
-    sem_t head_sem, tail_sem, queue_sem;
+    queue* q;
+    sem_t first_sem, last_sem, queue_sem;
     bool destroying;
 public:
     explicit MyMsgQueue();
@@ -14,5 +14,7 @@ public:
     int put(char* msg);
 
     int get(char *buffer, int buffer_len);
+
+    void drop();
 };
 
